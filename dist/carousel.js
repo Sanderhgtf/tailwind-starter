e9997821ccbb9751679f3c146171add4266a775b
BuildTools <Sanderhgtf@gmail.com>
2023-12-19 13:58:55 +0100

    It now pauses the carousel when hovered over

--- dist/carousel.js
+++ dist/carousel.js
@@ -2,6 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
   let currentIndex = 0;
   const numImages = 7;
   const imageContainer = document.getElementById('image-container');
+  const blueBackground = document.querySelector('.bg-blue-300');
+  let isHovered = false;
+
+  // Event listeners for hover
+  blueBackground.addEventListener('mouseenter', function () {
+    isHovered = true;
+  });
+
+  blueBackground.addEventListener('mouseleave', function () {
+    isHovered = false;
+  });
 
   function showSlide(index) {
     const imageWidth = document.querySelector('.carousel-image').offsetWidth;
@@ -11,18 +22,20 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function updateIndex(direction) {
-    currentIndex += direction;
+    if (!isHovered) {
+      currentIndex += direction;
 
-    // Reset index if it goes beyond the boundaries
-    if (currentIndex === numImages) {
-      currentIndex = 0;
-      imageContainer.style.transition = 'none';
+      // Reset index if it goes beyond the boundaries
+      if (currentIndex === numImages) {
+        currentIndex = 0;
+        imageContainer.style.transition = 'none';
+        showSlide(currentIndex);
+        imageContainer.offsetHeight; // Trigger reflow
+      }
+
+      imageContainer.style.transition = 'transform 0.5s ease-in-out';
       showSlide(currentIndex);
-      imageContainer.offsetHeight; // Trigger reflow
     }
-
-    imageContainer.style.transition = 'transform 0.5s ease-in-out';
-    showSlide(currentIndex);
   }
 
   // Automatic rotation
