032f322de5f9c2851ce1d0c4a71d8c44b4dea8e4
BuildTools <Sanderhgtf@gmail.com>
2023-12-19 13:45:49 +0100

    Took out the infinite scroll option of the carousel due to bugs

--- dist/carousel.js
+++ dist/carousel.js
@@ -1,12 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   let currentIndex = 0;
-  const numImages = 9;
+  const numImages = 7;
   const imageContainer = document.getElementById('image-container');
-
-  function initializeCarousel() {
-    const cloneImages = imageContainer.cloneNode(true);
-    imageContainer.appendChild(cloneImages);
-  }
 
   function showSlide(index) {
     const imageWidth = document.querySelector('.carousel-image').offsetWidth;
@@ -19,20 +14,16 @@ document.addEventListener('DOMContentLoaded', function () {
     currentIndex += direction;
 
     // Reset index if it goes beyond the boundaries
-    if (currentIndex === numImages * 2 - 1 || currentIndex === numImages - 1) {
+    if (currentIndex === numImages) {
+      currentIndex = 0;
       imageContainer.style.transition = 'none';
-      currentIndex = direction === 1 ? numImages : numImages * 2 - 2;
       showSlide(currentIndex);
       imageContainer.offsetHeight; // Trigger reflow
-      imageContainer.style.transition = 'transform 0.5s ease-in-out';
-      showSlide(currentIndex);
-    } else {
-      showSlide(currentIndex);
     }
-  }
 
-  // Initial setup
-  initializeCarousel();
+    imageContainer.style.transition = 'transform 0.5s ease-in-out';
+    showSlide(currentIndex);
+  }
 
   // Automatic rotation
   setInterval(() => {
